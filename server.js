2a74689d50daa59910216e723f034f1e324ec0c9
root <root@localhost.localdomain>
2019-01-21 03:28:30 -0500

    upload ulang

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express'),
+app = express();
 
-const app = express();
-
-//// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo/.home/home.component'));
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var server = app.listen(process.env.PORT || 8080);
